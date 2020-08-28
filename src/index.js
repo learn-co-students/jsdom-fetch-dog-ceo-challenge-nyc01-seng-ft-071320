c96376ab0cf3a4470f2b5784c6d0bf2a23328f2f
redazures <jwu_nyc@yahoo.com>
2020-08-28 08:23:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const getDogs = () => {
 function renderDog(dog){
     const dogDiv = document.createElement('div')
     const dogCollection = document.getElementById("dog-image-container")
-    dogDiv.innerHTML=`<img src ="${dog}" class="dog-pic" width:"20%">`
+    dogDiv.innerHTML=`<img src ="${dog}" class="dog-pic" width:"20px">`
     dogCollection.appendChild(dogDiv)
 }
 
@@ -40,27 +40,23 @@ function renderBreed(breed){
 function dogDropDown (){
     const dropdown = document.getElementById("breed-dropdown")
     dropdown.addEventListener('change', e => {
+        document.querySelectorAll('li').forEach(breed => {breed.style.display = 'block'})
         if (dropdown.value == "a" ) 
             {
-            document.querySelectorAll('li').forEach(breed => {if(breed.innerText[0] ==="a"){breed.style.display = 'block'}})
             document.querySelectorAll('li').forEach(breed => {if(breed.innerText[0] !=="a"){breed.style.display = 'none'}})
             };
         if (dropdown.value == "b" )
             {
-            document.querySelectorAll('li').forEach(breed => {if(breed.innerText[0] ==="b"){breed.style.display = 'block'}})
             document.querySelectorAll('li').forEach(breed => {if(breed.innerText[0] !=="b"){breed.style.display = 'none'}})
             };
         if (dropdown.value == "c" ) 
             {
-            document.querySelectorAll('li').forEach(breed => {if(breed.innerText[0] ==="c"){breed.style.display = 'block'}})
             document.querySelectorAll('li').forEach(breed => {if(breed.innerText[0] !=="c"){breed.style.display = 'none'}})
             };
         if (dropdown.value == "d" ) 
             {
-            document.querySelectorAll('li').forEach(breed => {if(breed.innerText[0] ==="d"){breed.style.display = 'block'}})
             document.querySelectorAll('li').forEach(breed => {if(breed.innerText[0] !=="d"){breed.style.display = 'none'}})
             };
         }
     );
-
 }
